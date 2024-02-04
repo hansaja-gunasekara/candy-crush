deb60d132a379754078ccaaed9bd0f1b07293dbc
hansaja-gunasekara <hgmgunasekara@gmail.com>
2024-02-04 22:30:30 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React, { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from './store/hooks'
-import { updateBoard } from './store'
-import { create } from 'domain';
-import { createBoard } from './utils/createBoard';
+import { useEffect } from "react";
+import Board from "./components/Board";
+import { moveBelow, updateBoard } from "./store";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { createBoard } from "./utils/createBoard";
+import {
+  formulaForColumnOfFour,
+  formulaForColumnOfThree,
+  generateInvalidMoves,
+} from "./utils/formulas";
+import {
+  checkForColumnOfThree,
+  checkForRowOfFour,
+  checkForRowOfThree,
+  isColumnOfFour,
+} from "./utils/moveCheckLogic";
 
-function App () {
-
+function App() {
   const dispatch = useAppDispatch();
-
-  const board = useAppSelector(({candyCrush: {board}}) => board);
-  const boardSize = useAppSelector(({candyCrush: {boardSize}}) => boardSize);
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
+  const boardSize = useAppSelector(
+    ({ candyCrush: { boardSize } }) => boardSize
+  );
 
   useEffect(() => {
-    dispatch(updateBoard(createBoard(Number(boardSize))));
-    }, [boardSize, dispatch]);
-  
+    dispatch(updateBoard(createBoard(boardSize)));
+  }, [dispatch, boardSize]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      checkForRowOfFour(
+        newBoard,
+        boardSize,
+        generateInvalidMoves(boardSize, true)
+      );
+      checkForColumnOfThree(
+        newBoard,
+        boardSize,
+        formulaForColumnOfThree(boardSize)
+      );
+      checkForRowOfThree(newBoard, boardSize, generateInvalidMoves(boardSize));
+      dispatch(updateBoard(newBoard));
+      dispatch(moveBelow());
+    }, 150);
+    return () => clearInterval(timeout);
+  }, [board, dispatch, boardSize]);
+
   return (
-    <div>App</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <Board />
+    </div>
+  );
 }
 
-export default App
-
+export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,34 @@
 import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
+import { dragEndReducer } from "./reducers/dragEnd";
+import { moveBelowReducer } from "./reducers/moveBelow";
 
 const initialState: {
-    board: String[];
-    boardSize: Number;
+    board: string[];
+    boardSize: number;
+    squareBeingReplaced: Element | undefined;
+    squareBeingDragged: Element | undefined;
 } = {
     board: [],
     boardSize: 8,
+    squareBeingDragged: undefined,
+    squareBeingReplaced: undefined,
 };
 
 const candyCrushSlice = createSlice({
     name: "candyCrush",
     initialState,
     reducers: {
-        updateBoard: (state, action: PayloadAction<String[]> ) => {
+        updateBoard: (state, action: PayloadAction<string[]> ) => {
             state.board = action.payload;
         },
+        dragStart: (state, action: PayloadAction<any>) => {
+            state.squareBeingDragged = action.payload;
+          },
+          dragDrop: (state, action: PayloadAction<any>) => {
+            state.squareBeingReplaced = action.payload;
+          },
+          dragEnd: dragEndReducer,
+        moveBelow: moveBelowReducer,
     },
 });
 
@@ -22,8 +36,12 @@ export const store = configureStore({
     reducer: {
         candyCrush: candyCrushSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
-export const { updateBoard } = candyCrushSlice.actions;
+export const { updateBoard, moveBelow, dragDrop, dragEnd, dragStart } = candyCrushSlice.actions;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
